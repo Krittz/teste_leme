4f400690d17df854d723d860b20ce980a7b836f6
Cristian Silva <cristian.silva@direcaomarcas.com.br>
2025-10-31 12:20:11 -0300

    feat<api>: Adicionando rotas para projeto

--- task-manager-api/src/Routes/api.php
+++ task-manager-api/src/Routes/api.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use App\Controllers\AuthController;
+use App\Controllers\ProjectController;
 use App\Controllers\UserController;
 use App\Database\Connection;
 use App\Helpers\ResponseHelper;
@@ -43,13 +44,28 @@ $router->group('/api', [new AuthMiddleware()], function ($router) {
 
 
     // Usuários
-    $router->group('/users', []. function($router) {
+    $router->group('/users', [] . function ($router) {
         $router->get('', [UserController::class, 'index']);
         $router->get('/{id}', [UserController::class, 'show']);
         $router->put('/{id}', [UserController::class, 'update']);
     });
 
     // Projetos
+    $router->group('/projects', [], function ($router) {
+        $router->get('', [ProjectController::class, 'index']);
+        $router->post('', [ProjectController::class, 'store']);
+        $router->get('/{id}', [ProjectController::class, 'show']);
+        $router->put('/{id}', [ProjectController::class, 'update']);
+        $router->delete('/{id}', [ProjectController::class, 'destroy']);
+
+        // Membros do projeto
+        $router->post('/{id}/members', [ProjectController::class, 'addMember']);
+        $router->delete('/{id}/members/{userId}', [ProjectController::class, 'removeMember']);
+        $router->get('/{id}/members', [ProjectController::class, 'getMembers']);
+
+        // Tarefas do projeto
+        $router->get('/{id}/tasks', [ProjectController::class, 'getTasks']);
+    });
 });
 
 
