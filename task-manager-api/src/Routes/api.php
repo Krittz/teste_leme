a91cf7bd3bbd9e1b4f323c16300d3ad81fc294af
Cristian Silva <cristian.silva@direcaomarcas.com.br>
2025-10-31 09:37:40 -0300

    feat<api>: Adicionando rotas de usuários

--- task-manager-api/src/Routes/api.php
+++ task-manager-api/src/Routes/api.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 use App\Controllers\AuthController;
+use App\Controllers\UserController;
 use App\Database\Connection;
 use App\Helpers\ResponseHelper;
 use App\Middlewares\AuthMiddleware;
@@ -19,10 +20,11 @@ use function PHPSTORM_META\map;
 
 // Rotas públicas
 $router->group('/api/auth', [], function ($router) {
+    // Registro de usuário
     $router->post('/register', [AuthController::class, 'register'], [
         new RateLimitMiddleware()
     ]);
-
+    // Login
     $router->post('/login', [AuthController::class], 'login', [
         new RateLimitMiddleware()
     ]);
@@ -31,12 +33,23 @@ $router->group('/api/auth', [], function ($router) {
 
 // Rotas protegidas
 $router->group('/api', [new AuthMiddleware()], function ($router) {
+    // Autenticação (rotas autenticadas)
     $router->group('/auth', [], function ($router) {
         $router->post('/logout', [AuthController::class, 'logout']);
         $router->get('/me', [AuthController::class, 'me']);
         $router->post('/refresh', [AuthController::class, 'refresh']);
         $router->post('/change-password', [AuthController::class, 'changePassword']);
     });
+
+
+    // Usuários
+    $router->group('/users', []. function($router) {
+        $router->get('', [UserController::class, 'index']);
+        $router->get('/{id}', [UserController::class, 'show']);
+        $router->put('/{id}', [UserController::class, 'update']);
+    });
+
+    // Projetos
 });
 
 
